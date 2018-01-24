a7ce57204f813e463450a9a6f74aa71e8f25e9ea
Bruno B. Biazin <bruno.biazin315@gmail.com>
2018-01-24 00:58:18 -0200

    Heroku fix

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 3001);
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
