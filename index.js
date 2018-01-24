a3bbd2d5db5b1c13d46004ee83bcc1d1496e8c89
Bruno B. Biazin <bruno.biazin315@gmail.com>
2018-01-24 01:00:24 -0200

    Fix

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 3000, function () {
+var port = process.env.PORT || 8000;
+
+app.listen(port, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
